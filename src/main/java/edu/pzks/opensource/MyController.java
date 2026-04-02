ea6fbe92efd1123c4a550e91fc05010d49ebe8d4
Oleksandr Ratsoi <san4es772@gmail.com>
2026-04-03 01:04:52 +0300

    make everything shorter

--- src/main/java/edu/pzks/opensource/MyController.java
+++ src/main/java/edu/pzks/opensource/MyController.java
@@ -7,18 +7,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/test")
 public class MyController {
-    @GetMapping("/hello/user")
-    public String helloUser(){
-        return "hello user";
-    }
+    @GetMapping("/user")
+    public String helloUser(){return "hello user";}
 
-    @GetMapping("/hello/admin")
-    public String helloAdmin(){
-        return "hello admin";
-    }
+    @GetMapping("/admin")
+    public String helloAdmin(){return "hello admin";}
 
-    @GetMapping("/hello/unknown")
-    public String helloUnknown(){
-        return "hello unknown";
-    }
+    @GetMapping("/unknown")
+    public String helloUnknown(){return "hello unknown";}
 }
